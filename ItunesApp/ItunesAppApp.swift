3425b9953ffbd23302fea4ae29c7231e13f2aab1
Alyona <alyonakisha1@rambler.ru>
2023-06-11 08:55:33 -0700

    Initial Commit

---
+++ ItunesApp/ItunesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesAppApp.swift
+//  ItunesApp
+//
+//  Created by MacBook Air on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ItunesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
